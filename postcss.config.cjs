27f5ee0c384284eca8bb8930c37f3c7c1c4cfe15
“Daniel” <daniel.fabian13x@gmail.com>
2022-12-28 21:29:09 +0100

    Added more PostCSS

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,18 @@
+// https://github.com/postcss/postcss-mixins
+// https://github.com/antyakushev/postcss-for
+// https://github.com/madyankin/postcss-each
+// https://github.com/postcss/postcss-nested
+// https://github.com/postcss/postcss-scss
+
 module.exports = {
+  syntax: 'postcss-scss',
   plugins: [
+    'postcss-mixins',
+    'postcss-for',
+    'postcss-each',
     'postcss-nested',
     'postcss-custom-media',
     'postcss-media-minmax',
+    'postcss-custom-selectors',
   ]
 }
