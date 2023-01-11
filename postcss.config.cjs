d585a13d4d44ef615045afb6096f684c588e77ce
“Daniel” <64810647+Xion-14@users.noreply.github.com>
2023-01-11 20:01:25 +0100

    Add support for inline comments

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,18 +2,21 @@
 // https://github.com/antyakushev/postcss-for
 // https://github.com/madyankin/postcss-each
 // https://github.com/postcss/postcss-nested
-// https://github.com/postcss/postcss-scss
 // https://github.com/postcss/postcss-simple-vars
 // https://github.com/nathanhood/postcss-variables
 // https://github.com/andyjansson/postcss-conditionals
 // https://github.com/csstools/postcss-custom-selectors
+// https://github.com/postcss/postcss-scss
+// https://github.com/mummybot/postcss-strip-inline-comments
 
 // Fork of `postcss-simple-vars` to fix issue with `@each`
 // @ts-ignore
 const simpleVariables = require('./custom-postcss/postcss-simple-vars/index.js')
 
 module.exports = {
+  syntax: 'postcss-scss',
   plugins: [
+    'postcss-strip-inline-comments',
     simpleVariables,
     'postcss-conditionals',
     'postcss-mixins',
